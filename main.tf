fd59f61a3f5311d7c11da104940c2946ffc229a5
mukesh-searce <47624366+mukesh-searce@users.noreply.github.com>
2019-10-28 20:38:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,20 +6,20 @@ provider "spotinst" {
 # Create an Elastigroup
 resource "spotinst_elastigroup_aws" "default-elastigroup" {
 
-  name        = "ecs-elastigroup"
+  name        = "spotinst-elastigroup"
   description = "created by Terraform"
   product     = "Linux/UNIX"
 
-  max_size          = 3
+  max_size          = 10
   min_size          = 1
-  desired_capacity  = 2
+  desired_capacity  = 3
   capacity_unit     = "weight"
 
-  region      = "${var.aws_region}"
+  region      = "us-east-1"
   subnet_ids  = ["subnet-91da33cd", "subnet-7751ec3d", "subnet-411ef56f"]
 
   image_id              = "ami-0361adb513f74d321"
-  iam_instance_profile  = "test-ecs-role"
+  iam_instance_profile  = "test-ecs-mukesh-role"
   key_name              = "aws-general-key"
   security_groups       = ["sg-05fc95ed5f04eb9a3"]
   user_data             = ""
@@ -29,23 +29,23 @@ resource "spotinst_elastigroup_aws" "default-elastigroup" {
 
   instance_types_ondemand       = "t3.micro"
   instance_types_spot           = ["c4.large", "r4.large"]
-  instance_types_preferred_spot = ["t2.large"]
+  # instance_types_preferred_spot = ["t3.micro"]
 
   instance_types_weights {
     instance_type = "r4.large"
-    weight        = 5
+    weight        = 1
   }
 
   instance_types_weights {
     instance_type = "c4.large"
-    weight        = 3
+    weight        = 5
   }
 
   orientation           = "balanced"
   fallback_to_ondemand  = false
   cpu_credits           = "unlimited"
 
-  wait_for_capacity         = 5
+  wait_for_capacity         = 1
   wait_for_capacity_timeout = 300
 
   scaling_strategy {
@@ -53,54 +53,10 @@ resource "spotinst_elastigroup_aws" "default-elastigroup" {
     termination_policy = "default"
   }
 
-  scaling_up_policy {
-    policy_name = "policy-name"
-    metric_name = "CPUUtilization"
-    namespace   = "AWS/EC2"
-    source      = "cloudWatch"
-    statistic   = "average"
-    unit        = "percent"
-    cooldown    = 60
-    is_enabled  = false
-
-    threshold          = 10
-    operator           = "gte"
-    evaluation_periods = 10
-    period             = 60
-
-    action_type = "updateCapacity"
-    minimum     = 0
-    maximum     = 10
-    target      = 5
-
-  }
-
-  scaling_down_policy {
-    policy_name = "policy-name"
-    metric_name = "CPUUtilization"
-    namespace   = "AWS/EC2"
-    source      = "cloudWatch"
-    statistic   = "average"
-    unit        = "percent"
-    cooldown    = 60
-    is_enabled  = false
-
-    threshold          = 10
-    operator           = "lte"
-    evaluation_periods = 10
-    period             = 60
-
-    action_type = "updateCapacity"
-    minimum     = 0
-    maximum     = 10
-    target      = 5
-
-  }
-
   tags {
      key   = "Env"
      value = "dev"
-  }
+  } 
 
   lifecycle {
     ignore_changes = [
